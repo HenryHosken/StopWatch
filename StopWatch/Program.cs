a53c7c903e3b534c973626b1b3d52a86d473f1c6
henry hosken <henryhosken13@gmail.com>
2023-08-24 16:31:54 -0300

    Add project files.

---
+++ StopWatch/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace StopWatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundos => 10S");
+            Console.WriteLine("M = Minuto => 1M");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo Deseja contar ?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time ==  0)
+                Environment.Exit(0);
+
+            Start(time * multiplier);
+        }
+        static void Start(int time)
+        {
+            
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado.");
+            Thread.Sleep(2500);
+        }
+    }
+}
